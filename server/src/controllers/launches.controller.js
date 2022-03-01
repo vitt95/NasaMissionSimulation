5e285bbd8794cfcf6fcdce6fceb2ab38714be817
vitt95 <vittorioaiello95@gmail.com>
2022-03-01 22:09:49 +0100

    minor fixes

--- server/src/controllers/launches.controller.js
+++ server/src/controllers/launches.controller.js
@@ -6,12 +6,22 @@ function httpGetAllLaunches(req, res){
             .json(getAllLaunches());
 }
 
+function isValidLaunch(launch){
+    return (launch.mission && launch.rocket && launch.launchDate && launch.destination);
+}
+
 function httpAddNewLaunch(req, res){
     const launch = req.body;
-    launch.launchDate = new Date(launch.launchDate);
+    
+    if(isValidLaunch(launch)){
+        launch.launchDate = new Date(launch.launchDate);
 
-    addNewLaunch(launch);
-    return res.status(200).json(launch);
+        addNewLaunch(launch);
+        return res.status(200).json(launch);
+    }
+
+    return res.status(400).json();
+
 }
 
 module.exports = {
